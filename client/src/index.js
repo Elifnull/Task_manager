0760d663b7ffc7d00e5ad7604e6885596d96760e
Serge Chvorun <elifnull@protonmail.com>
2022-04-20 22:24:54 -0400

    bug fix: depdencies and reactDom

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

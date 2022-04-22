6c3c6c32846dcadaa302cc3386f3288c10930ec6
ColePetrovics <petrovicscole@outlook.com>
2022-04-22 16:30:09 -0500

    Update task.routes.js

--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -5,7 +5,7 @@ module.exports = (app) =>{
     app.put('/api/update/:id', TaskController.updateTask);
     app.delete('/api/delete/:id', TaskController.deleteTask);
     app.get('/api/task/:id', TaskController.getOneTask);
-    app.get('/alltasks', TaskController.getAllTasks);
+    app.get('api/alltasks', TaskController.getAllTasks);
     app.get('/api/myownedtasks/:username"', TaskController.getMyOwnedTasks);
     // app.get('/myassignedtasks', TaskController.getMyAssignedTasks);
 }

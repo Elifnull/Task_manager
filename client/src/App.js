f063f8ac7cfccff0a87216c3effff9b33d52a7da
Serge Chvorun <elifnull@protonmail.com>
2022-04-22 17:02:21 -0400

    resolved App.js path issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react";
-import './App.css';
+// import './App.css';
 import Header from "./components/Header";
 import {useNavigate} from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";

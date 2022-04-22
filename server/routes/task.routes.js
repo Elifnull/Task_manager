f762b26883b29294a10c4b849c0c8338584dd2f8
ColePetrovics <petrovicscole@outlook.com>
2022-04-22 16:27:15 -0500

    Update task.routes.js

--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -1,11 +1,11 @@
 const TaskController = require("../controllers/task.controller");
 
 module.exports = (app) =>{
-    app.get('/alltasks', TaskController.getAllTasks);
-    app.get('/myassignedtasks', TaskController.getMyAssignedTasks);
-    app.get('/api/myownedtasks', TaskController.getMyOwnedTasks);
     app.post('/api/create', TaskController.createTask);
     app.put('/api/update/:id', TaskController.updateTask);
     app.delete('/api/delete/:id', TaskController.deleteTask);
     app.get('/api/task/:id', TaskController.getOneTask);
+    app.get('/alltasks', TaskController.getAllTasks);
+    app.get('/api/myownedtasks/:username"', TaskController.getMyOwnedTasks);
+    // app.get('/myassignedtasks', TaskController.getMyAssignedTasks);
 }
